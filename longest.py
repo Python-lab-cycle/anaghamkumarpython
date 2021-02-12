f4e2466ce7811d1fd7b4aaa088804b8fb306acea
anaghamkumar <76676321+anaghamkumar@users.noreply.github.com>
2021-02-12 22:18:29 +0530

    Delete longest.py

--- longest.py
+++
@@ -1,9 +0,0 @@
-lis=input("Enter a list with some string (space separated) :")
-words_list=lis.split()
-word_len = []
-for n in words_list:
-    word_len.append((len(n), n)
-word_len.sort()
-word_len.sort()
-print(word_len)
-print("Longest word:",word_len[-1][1], "and length=",word_len[-1][0])
