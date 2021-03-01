2e3acf0da133c4a58e02ab67aca878048ca34d55
anaghamkumar <76676321+anaghamkumar@users.noreply.github.com>
2021-03-01 16:32:15 +0530

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,11 @@
+import csv
+csv_col={'ID';1.'Name':'Alex','Place':'Thrissur'},
+{'ID':2,'Name':'Ben','Place':'Palakad'},
+{'ID':3,'Name':'shri Ram','Place':'Kottayam'}
+{'ID':4,'Name':'Rama','Place':'Wayanad'}
+{'ID':5,'Name':'Selmi','Place':'Kozhikode'}
+csv_file="Name.csv"
+try:
+     with open(csv_file,'w')as file1:
+         writer=csv.DictWriter
+    
